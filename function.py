eb1a6959ff1f98f6969c6c50ea7da0fb62531c6e
TaylanKaray <95767399+TaylanKaray@users.noreply.github.com>
2021-12-08 21:29:35 +0300

    Create function.py

---
+++ function.py
@@ -0,0 +1,36 @@
+def Kıyafet_listele(yol,listele):
+  uygula=yol.cursor()
+  komut=liste
+  uygula.execute(komut)
+  return uygula
+
+
+def Kıyafet_ekle(yol,ekle):
+uygula2=yol.cursor()
+komut2=ekleme
+uygula2.execute(komut2)
+return uygula2
+
+def veri_silme(yol,guncelle):
+  uygula3=yol.cursor()
+  komut3=silme
+  uygula3.execute(komut3)
+  return uygula3
+
+def veri_guncelleme(yol,guncelle):
+  uygula4=yol.cursor()
+  komut4=guncelle
+  uygula3.execute(komut4)
+  return uygula4
+
+def Kıyafet_getir(yol,ayıklama):
+  uygula5=yol.cursor()
+  ayıklama=ayıklama
+  uygula5.execute(uygulama)
+  return uygulama5
+
+def veri_guncelleme(yol,guncelle):
+  uygula4=yol.cursor()
+  komut4=guncelle
+  uygula3.execute(komut4)
+  return uygula4
